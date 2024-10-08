863d19bf805822ba81b1bd010abed9a65d5c6746
TL <tlgamer.gaming@gmail.com>
2024-10-08 15:50:45 +0300

    created hash function

---
+++ requirements.py
@@ -0,0 +1,15 @@
+
+
+import hashlib
+
+def hash_string(string):
+    
+    hash_object = hashlib.sha256()
+
+    
+    hash_object.update(string.encode('utf-8'))
+
+    # Get the hashed string in hexadecimal form
+    hashed_string = hash_object.hexdigest()
+
+    return hashed_string
