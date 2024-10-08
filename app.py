934f334d812b2beee122379400ac0ae0cbb24447
TL <tlgamer.gaming@gmail.com>
2024-10-08 13:44:02 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, flash, redirect, render_template, request, session
+import sqlite3
+
+
+
+
+app= Flask(__name__)
+
+@app.route('/')
+def main():
+    
+
+    return render_template("index.html")
+
+
+@app.route('/login')
+def login():
+    
+    session.clear()
+
+    if request.method == "POST":
+            1==1
+
+
+@app.route('/register')
+def register():
+     
+     return render_template("register.html")
+
+
+    
+    
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
